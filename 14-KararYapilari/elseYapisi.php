fefff4a74221227e8a33b1cdd3e644b8fdacfcba
Berkcan Gümüşışık <gumusisikberkcan@gmail.com>
2021-10-19 06:18:41 +0300

    karar yapılarına devam edildi

--- 14-KararYapilari/elseYapisi.php
+++ 14-KararYapilari/elseYapisi.php
@@ -1,5 +1,36 @@
 <?php 
 
+$mevsim = "Kış";
+if($mevsim == "İlkbahar"){
+  echo "Mevsim ilkbahardır.";
+
+}
+else if ($mevsim == "Yaz"){
+  echo"Mevsim yazdır";
+}
+else if ($mevsim == "Sonbahar"){
+  echo"Mevsim sonbahardır";
+}
+else if ($mevsim == "Kış"){
+  echo"Mevsim kıştır";
+}
+else{
+  echo "Yanlış değer girişi yaptınız";
+}
+
+echo "<br>";
+$sayi = 0;
+if($sayi > 5):
+  echo "Sayı 5'ten büyüktür";
+elseif($sayi == 0):
+  echo "Sayı 0'dır";
+else:
+  echo "Sayı 5'ten küçük ve 0 değildir";
+endif;
+
+echo "<br>";
+$sayi = 35;
+echo($sayi > 10) ? "Sayı 10'dan büyük":"Sayı 10'dan küçük";
 
 /*
     Else(değilse yapısı)
@@ -13,6 +44,9 @@
       else{
           //hiçbir koşul sağlanmaz.
       }
+
+      Ternary(Üçlü operatör)
+      ifade(koşul)?(doğruysa):(yalnışsa);
 */
 
 ?>
